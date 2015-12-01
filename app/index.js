3539e767c96f0dd0fe06a30eaaa9f1c5d1ddad48
Ricky Miller <ricky.miller@gmail.com>
2015-11-30 23:21:19 -0500

    Add three.js version

--- app/index.js
+++ app/index.js
@@ -1,36 +1,81 @@
+/*global THREE requestAnimationFrame*/
 'use strict'
 
 const binCount = 256
 const SpectrumAnalyzer = require('spectrum-analyzer')
 const analyzer = new SpectrumAnalyzer('https://raw.githubusercontent.com/rickycodes/tones/master/futurecop.mp3', binCount, 0.80)
-const lines = []
+
+var camera, group, scene, renderer
 
 function animate () {
+  render()
+  requestAnimationFrame(animate)
+}
+
+function render () {
   var data = analyzer.getFrequencyData()
   analyzer.updateSample()
-  lines.forEach(function (line, i) {
-    line.style.height = data[i] + 'px'
+  camera.lookAt(scene)
+  group.children.forEach(function (child, i) {
+    if (data[i] !== 0) {
+      child.scale.z = data[i] * 0.08
+    }
   })
-  requestAnimationFrame(animate)
+  renderer.render(scene, camera)
 }
 
 function setup () {
   var xPos = 0
-  const width = 5
-  const space = 0
-  const target = document.getElementsByClassName('container')[0]
-  for (var i = 0; i < binCount; i++) {
-    var line = document.createElement('div')
-    line.setAttribute('class','line line_' + i)
-    line.style.left = xPos + 'px'
-    line.style.width = width + 'px'
-    lines.push(line)
-    target.appendChild(line)
+  var yPos = 0
+  const width = 100
+  const space = width + 40
+
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000)
+  group = new THREE.Object3D()
+  scene = new THREE.Scene()
+
+  camera.position.z = 5000
+
+  var light = new THREE.HemisphereLight(0xffffff, 1)
+  group.add(light)
+
+  var geometry = new THREE.BoxGeometry(width, width, width)
+
+  var color = 0x001EFF
+
+  console.log(color)
+
+  for (var i = 1; i < binCount; i++) {
+    var material = new THREE.MeshPhongMaterial({ color: i * color })
+    var object = new THREE.Mesh(geometry, material)
+
+    object.position.x = xPos
+    object.position.y = yPos
+
     xPos += width + space
+
+    if ((i % Math.sqrt(binCount)) === 0) {
+      xPos = 0
+      yPos -= width + space
+    }
+
+    group.add(object)
   }
+
+  group.position.set(-2000, 1600, 0)
+  group.rotation.x = -0.80
+
+  scene.add(group)
+
+  renderer = new THREE.WebGLRenderer({alpha: true})
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.sortObjects = false
+
+  document.body.appendChild(renderer.domElement)
 }
 
-(function (){
+(function () {
   setup()
   animate()
 })()
